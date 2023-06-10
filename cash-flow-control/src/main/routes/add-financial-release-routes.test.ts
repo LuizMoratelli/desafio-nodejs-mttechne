96866a7b94066fb16f162e5f198b6619319c8cc2
Luiz Moratelli <la.moratelli@gmail.com>
2023-06-10 17:29:25 -0300

    refactor(add-financial-release-routes): add mongodb helper

--- cash-flow-control/src/main/routes/add-financial-release-routes.test.ts
+++ cash-flow-control/src/main/routes/add-financial-release-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest';
 import app from '../config/app';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongodb-helper';
 
 describe('AddFinancialRelease Routes', () => {
+  beforeEach(async () => {
+    const financialReleaseCollection = await MongoHelper.getCollection(
+      'financial-releases'
+    );
+    await financialReleaseCollection.deleteMany({});
+  });
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '');
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
   test('Should return an financial release on success', async () => {
     await request(app)
       .post('/api/financial-release')
