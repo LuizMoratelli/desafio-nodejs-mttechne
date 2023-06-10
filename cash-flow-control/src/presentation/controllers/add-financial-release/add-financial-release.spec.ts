2b1182141beda7b21810119155dbd53e7bfa1c6e
Luiz Moratelli <la.moratelli@gmail.com>
2023-06-10 09:59:10 -0300

    test(financial-release): ensure required parameters are defined

--- cash-flow-control/src/presentation/controllers/add-financial-release/add-financial-release.spec.ts
+++ cash-flow-control/src/presentation/controllers/add-financial-release/add-financial-release.spec.ts
@@ -15,4 +15,34 @@ describe('AddFinancialRelease Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: value'));
   });
+
+  test('Should return 400 if no type is provided', () => {
+    const sut = new AddFinancialReleaseController();
+    const httpRequest = {
+      body: {
+        description: 'Venda',
+        value: 100.0,
+        date: '2023-06-10',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: type'));
+  });
+
+  test('Should return 400 if no date is provided', () => {
+    const sut = new AddFinancialReleaseController();
+    const httpRequest = {
+      body: {
+        description: 'Venda',
+        type: 'Entrada',
+        value: 100.0,
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: date'));
+  });
 });
