a9155d00fea8b3756d73b78ba4e9fc3d973df792
Luiz Moratelli <la.moratelli@gmail.com>
2023-06-10 15:51:20 -0300

    chore(express): add express

--- cash-flow-control/src/data/usecases/add-financial-release/db-add-financial-release.protocols.ts
+++ cash-flow-control/src/data/usecases/add-financial-release/db-add-financial-release.protocols.ts
@@ -1,4 +1,3 @@
 export * from '../../../domain/models/financial-release';
-export * from '../../../domain/models/financial-release.type';
 export * from '../../../domain/usecases/add-financial-release';
 export * from '../../protocols/add-financial-release';
