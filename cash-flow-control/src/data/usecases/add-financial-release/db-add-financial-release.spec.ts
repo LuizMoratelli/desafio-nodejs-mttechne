be904d6eed6c6deb3a9ed21f51452e349ac48393
Luiz Moratelli <la.moratelli@gmail.com>
2023-06-10 13:47:08 -0300

    test(add-financial-release): ensure usecase calls repository

--- cash-flow-control/src/data/usecases/add-financial-release/db-add-financial-release.spec.ts
+++ cash-flow-control/src/data/usecases/add-financial-release/db-add-financial-release.spec.ts
@@ -1,13 +1,47 @@
 import { DbAddFinancialRelease } from './db-add-financial-release';
-import { FinancialReleaseType } from './db-add-financial-release.protocols';
+import {
+  AddFinancialReleaseModel,
+  AddFinancialReleaseRepository,
+  FinancialReleaseModel,
+  FinancialReleaseType,
+} from './db-add-financial-release.protocols';
 
-const makeSut = (): DbAddFinancialRelease => {
-  return new DbAddFinancialRelease();
+interface SutTypes {
+  sut: DbAddFinancialRelease;
+  addFinancialReleaseRepository: AddFinancialReleaseRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const addFinancialReleaseRepository = makeAddFinancialReleaseRepository();
+  const sut = new DbAddFinancialRelease(addFinancialReleaseRepository);
+
+  return {
+    sut,
+    addFinancialReleaseRepository,
+  };
+};
+
+const makeAddFinancialReleaseRepository = (): AddFinancialReleaseRepository => {
+  class AddFinancialReleaseRepositoryStub
+    implements AddFinancialReleaseRepository
+  {
+    async add(_: AddFinancialReleaseModel): Promise<FinancialReleaseModel> {
+      const fakeFinancialRelease = {
+        id: 'valid_id',
+        description: 'valid_description',
+        value: 100.0,
+        type: 'Entrada' as FinancialReleaseType,
+        date: '2023-06-10',
+      };
+      return new Promise((resolve) => resolve(fakeFinancialRelease));
+    }
+  }
+  return new AddFinancialReleaseRepositoryStub();
 };
 
 describe('DbAddFinancialRelease Usecase', () => {
   test('Should resolves', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const financialReleaseData = {
       description: 'valid_description',
       value: 100.0,
@@ -16,5 +50,18 @@ describe('DbAddFinancialRelease Usecase', () => {
     };
 
     expect(sut.add(financialReleaseData)).resolves;
+  });
+
+  test('Should call AddFinancialRelaseRepository with correct values', async () => {
+    const { sut, addFinancialReleaseRepository } = makeSut();
+    const addSpy = jest.spyOn(addFinancialReleaseRepository, 'add');
+    const data = {
+      description: 'valid_description',
+      value: 100.0,
+      type: 'Entrada' as FinancialReleaseType,
+      date: '2023-06-10',
+    };
+    await sut.add(data);
+    expect(addSpy).toHaveBeenCalledWith(data);
   });
 });
