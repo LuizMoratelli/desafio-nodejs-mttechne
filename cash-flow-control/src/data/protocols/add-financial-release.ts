7561e12d3a727cf374e16e31fc1671b4e9a12f66
Luiz Moratelli <la.moratelli@gmail.com>
2023-06-10 13:40:57 -0300

    feat(add-financial-release): ensure usecase calls repository

---
+++ cash-flow-control/src/data/protocols/add-financial-release.ts
@@ -0,0 +1,8 @@
+import { FinancialReleaseModel } from '../../domain/models/financial-release';
+import { AddFinancialReleaseModel } from '../../domain/usecases/add-financial-release';
+
+export interface AddFinancialReleaseRepository {
+  add: (
+    accountData: AddFinancialReleaseModel
+  ) => Promise<FinancialReleaseModel>;
+}
--- cash-flow-control/src/data/usecases/add-financial-release/db-add-financial-release.ts
+++ cash-flow-control/src/data/usecases/add-financial-release/db-add-financial-release.ts
@@ -1,13 +1,18 @@
 import {
   AddFinancialRelease,
   AddFinancialReleaseModel,
+  AddFinancialReleaseRepository,
   FinancialReleaseModel,
 } from './db-add-financial-release.protocols';
 
 export class DbAddFinancialRelease implements AddFinancialRelease {
-  constructor() {}
+  constructor(
+    private readonly addFinancialReleaseRepository: AddFinancialReleaseRepository
+  ) {}
 
-  async add({}: AddFinancialReleaseModel): Promise<FinancialReleaseModel> {
+  async add(data: AddFinancialReleaseModel): Promise<FinancialReleaseModel> {
+    await this.addFinancialReleaseRepository.add(data);
+
     return new Promise((resolve) =>
       resolve({} as unknown as FinancialReleaseModel)
     );
