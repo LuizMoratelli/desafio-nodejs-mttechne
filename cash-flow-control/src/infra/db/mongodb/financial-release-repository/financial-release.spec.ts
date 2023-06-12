5f8fdc2b6f6a962aacedfb8ad877dd1a6c0ebc37
Luiz Moratelli <la.moratelli@gmail.com>
2023-06-12 10:47:41 -0300

    test(financial-release-repository): add tests for getAll, update and delete

--- cash-flow-control/src/infra/db/mongodb/financial-release-repository/financial-release.spec.ts
+++ cash-flow-control/src/infra/db/mongodb/financial-release-repository/financial-release.spec.ts
@@ -53,4 +53,70 @@ describe('FinancialRelease Mongo Repository', () => {
     expect(financialRelease?.type).toBe(FinancialReleaseType.ENTRADA);
     expect(financialRelease?.value).toBe(100.0);
   });
+
+  test('Should return an financialRelease on getAll', async () => {
+    const sut = new FinancialReleaseMongoRepository();
+
+    await sut.add({
+      date: '2023-06-10',
+      description: 'valid_description',
+      type: FinancialReleaseType.ENTRADA,
+      value: 100.0,
+    });
+
+    await sut.add({
+      date: '2023-06-11',
+      description: 'valid_description',
+      type: FinancialReleaseType.ENTRADA,
+      value: 100.0,
+    });
+
+    const financialRelease = await sut.getAll();
+
+    expect(financialRelease).toBeDefined();
+    expect(financialRelease).toHaveLength(2);
+  });
+
+  test('Should return an financialRelease on update', async () => {
+    const sut = new FinancialReleaseMongoRepository();
+
+    const createdFinancialRelease = await sut.add({
+      date: '2023-06-10',
+      description: 'valid_description',
+      type: FinancialReleaseType.ENTRADA,
+      value: 100.0,
+    });
+
+    const updatedFinancialRelease = await sut.update({
+      id: createdFinancialRelease.id,
+      date: '2023-06-11',
+      description: 'updated_valid_description',
+      type: FinancialReleaseType.SAIDA,
+      value: 120.0,
+    });
+
+    expect(updatedFinancialRelease).toBeDefined();
+    expect(updatedFinancialRelease?.id).toBeTruthy();
+    expect(updatedFinancialRelease?.date).toBe('2023-06-11');
+    expect(updatedFinancialRelease?.description).toBe('updated_valid_description');
+    expect(updatedFinancialRelease?.type).toBe(FinancialReleaseType.SAIDA);
+    expect(updatedFinancialRelease?.value).toBe(120.0);
+  });
+
+  test('Should return undefined on delete', async () => {
+    const sut = new FinancialReleaseMongoRepository();
+
+    const createdFinancialRelease = await sut.add({
+      date: '2023-06-10',
+      description: 'valid_description',
+      type: FinancialReleaseType.ENTRADA,
+      value: 100.0,
+    });
+
+    const updatedFinancialRelease = await sut.delete({
+      id: createdFinancialRelease.id,
+    });
+
+    expect(updatedFinancialRelease).toBeUndefined();
+  });
 });
